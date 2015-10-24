2f1da79d8c2c88214905e5605aa36322a2baccd4
Marc Høegh <ebax_90@hotmail.com>
2015-10-24 13:35:17 +0200

    Added session store and logger

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
 let express = require('express')
 let app = express()
-let podioJS = require('podio-js').api;
+let podioJS = require('podio-js').api
+let winston = require('winston')
+let path = require('path')
+let fs = require('fs')
 
 // Config
+let sessionStore = {
+  get(authType, callback) {
+
+    let filePath = path.join(__dirname, 'sessionStore.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+
+      if (err && err.code !== 'ENOENT') {
+        throw err
+      } else if (err) {
+        callback()
+      } else {
+        callback(JSON.parse(data))
+      }
+    })
+  },
+
+  set(podioOauth, authType, callback) {
+
+    let filePath = path.join(__dirname, 'sessionStore.json');
+    fs.writeFile(filePath, JSON.stringify(podioOauth), err => {
+
+      if (err) throw err;
+      callback()
+    })
+  }
+}
+
+winston.level = 'debug'
 const PORT = 8000
 let podio = new podioJS({
   authType: 'server',
   clientId: 'oauthtesting',
   clientSecret: '1JFBMVraP9MtogCGTZHV2AKbYWmm37qVKXUI70P0v9DTbAlT21R5CYriY5AjiPYN'
 }, {
-  apiURL: 'http://api.nextpodio.dk'
+  apiURL: 'http://api.nextpodio.dk',
+  sessionStore: sessionStore
 })
 
 const redirecUrl = 'http://localhost:8000/callback'
@@ -22,21 +55,15 @@ const config = {
 app.engine('ejs', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views/')
-
 app.use('/client', express.static('src/client'));
 
 // Middleware
 app.use((req, res, next) => {
 
-  if (req.url === '/auth') {
-    return next()
-  }
+  if (req.url === '/auth') return next();
 
   podio.isAuthenticated()
-  .then(() => {
-
-    next()
-  })
+  .then(next)
   .catch(err => {
     if (req.query && req.query.code) {
       next()
@@ -50,7 +77,6 @@ app.get('/', (req, res) => {
   podio.request('GET', '/org/').then(result => {
     res.render('dashboard')
   })
-
 })
 
 app.get('/callback', (req, res) => {
@@ -66,17 +92,16 @@ app.get('/auth', (req, res) => {
 app.get('/proxy*', (req, res) => {
   let url = req.params[0]
   let { method } = req
-  console.log(url, method)
+  winston.log('debug', url, method)
   podio.request(req.method, url).then(result => {
-    console.log(result)
-    res.send(result)
+    res.json(result)
   })
   .catch(err => {
-    console.log(err)
-    res.error(err)
+    res.status(err.status)
+    res.send(err)
   })
 })
 
 app.listen(8000, () => {
-  console.log(`Listening on ${PORT}`)
+  winston.info('debug', `Listening on ${PORT}`)
 })
