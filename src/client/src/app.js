c2eef375785c3ccbd43e394b7777876da16d6482
Marc Høegh <ebax_90@hotmail.com>
2015-11-03 19:15:23 +0100

    Bit of styling

--- src/client/src/app.js
+++ src/client/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import Connect from './components/Connect';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import * as routes from './components/Routes';
 
 let rootElement = document.getElementById('root-element');
@@ -11,13 +11,13 @@ if (rootElement) {
   render(
     <Router>
       <Route path="/" component={Root}>
-        <Route path="apps" component={routes.Apps} />
-        <Route path="orgs" component={routes.Orgs} />
-        <Route path="conversations" component={routes.Conversations} />
-        <Route path="users" component={routes.Users} />
-        <Route path="search" component={routes.Search} />
-        <Route path="spaces" component={routes.Spaces} />
-        <Route path="tasks" component={routes.Tasks} />
+        <Route path="apps" name="apps" component={routes.Apps} />
+        <Route path="orgs" name="orgs" component={routes.Orgs} />
+        <Route path="conversations" name ="conversations" component={routes.Conversations} />
+        <Route path="users" name ="users" component={routes.Users} />
+        <Route path="search" name="search" component={routes.Search} />
+        <Route path="spaces" name="spaces" component={routes.Spaces} />
+        <Route path="tasks" name ="tasks" component={routes.Tasks} />
       </Route>
     </Router>
     , rootElement
--- src/client/src/components/Root.js
+++ src/client/src/components/Root.js
@@ -7,13 +7,13 @@ export default class Root extends Component {
     return (
       <div className="root">
         <ul>
-          <li><Link to="/apps">Apps</Link></li>
-          <li><Link to="/orgs">Orgs</Link></li>
-          <li><Link to="/conversations">Conversations</Link></li>
-          <li><Link to="/users">Users</Link></li>
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/spaces">Spaces</Link></li>
-          <li><Link to="/tasks">Tasks</Link></li>
+          <li><Link to="apps">Apps</Link></li>
+          <li><Link to="orgs">Orgs</Link></li>
+          <li><Link to="conversations">Conversations</Link></li>
+          <li><Link to="users">Users</Link></li>
+          <li><Link to="search">Search</Link></li>
+          <li><Link to="spaces">Spaces</Link></li>
+          <li><Link to="tasks">Tasks</Link></li>
         </ul>
         { this.props.children }
       </div>
